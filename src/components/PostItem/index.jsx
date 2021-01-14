9fe40825c9e2c1a73f7d318d961222014a055625
ydroulis <lombardi.droulis@gmail.com>
2021-01-14 20:06:34 -0300

    style: aplying the style in the posts pages

--- src/components/PostItem/index.jsx
+++ src/components/PostItem/index.jsx
@@ -8,7 +8,7 @@ const PostItem = ({ slug, background, category, date, timeToRead, title, descrip
     <S.PostItemWrapper>
       <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>{date} - {timeToRead} min de leitura</S.PostItemDate>
+        <S.PostItemDate>{date} • {timeToRead} min de leitura</S.PostItemDate>
         <S.PostItemTitle>{title}</S.PostItemTitle>
         <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
