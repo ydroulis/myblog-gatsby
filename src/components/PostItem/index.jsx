5d2d52c0f28ae2e1108732c0841b2ad47380ee90
ydroulis <lombardi.droulis@gmail.com>
2021-01-14 19:10:04 -0300

    my blog

--- src/components/PostItem/index.jsx
+++ src/components/PostItem/index.jsx
@@ -8,7 +8,7 @@ const PostItem = ({ slug, background, category, date, timeToRead, title, descrip
     <S.PostItemWrapper>
       <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>{date} * {timeToRead}</S.PostItemDate>
+        <S.PostItemDate>{date} - {timeToRead} min de leitura</S.PostItemDate>
         <S.PostItemTitle>{title}</S.PostItemTitle>
         <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
