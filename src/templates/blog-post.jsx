6275e598f57c98e75e4132fbe6ab4bb366d81e8e
ydroulis <lombardi.droulis@gmail.com>
2021-02-18 23:49:49 -0300

    feat(templates): add Comments component

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -4,6 +4,7 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 import RecomendedPosts from "../components/recomendedPosts";
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styled";
 
@@ -26,6 +27,8 @@ const BlogPost = ({ data, pageContext }) => {
               <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
             </S.MainContent>
             <RecomendedPosts next={next} previous={previous}/>
+            <Comments url={post.fields.slug} title={post.frontmatter.title}/>
+            
         </Layout>
     )
 }
@@ -33,6 +36,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
 query Post($slug: String!) {
     markdownRemark(fields: {slug: {eq: $slug}}) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
