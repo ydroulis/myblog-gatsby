0a139aabb4068f308144e4c89b0845f7cbf03ed6
ydrouulis <lombardi.droulis@gmail.com>
2021-02-20 02:18:52 -0300

    feat(components/search): create Hit component

---
+++ src/components/Search/Hit.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PostItem from '../PostItem'
+
+const Hit = ({ hit }) => {
+    return(
+        <PostItem 
+            slug={hit.fields.slug}
+            background={hit.background}
+            title={hit.title}
+            date={hit.date}
+            description={hit.description}
+            category={hit.category}
+        />
+    )
+}
+
+export default Hit
