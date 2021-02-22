e89b40726d14d89dacab421b2b214636d3cc852f
ydrouulis <lombardi.droulis@gmail.com>
2021-02-22 01:59:50 -0300

    feat(components/menubar): light and grid mode

--- src/components/Menubar/index.jsx
+++ src/components/Menubar/index.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 
-import {Home} from "@styled-icons/boxicons-solid/Home";
-import {SearchAlt2 as Search} from "@styled-icons/boxicons-regular/SearchAlt2";
-import {UpArrowAlt as Arrow} from "@styled-icons/boxicons-regular/UpArrowAlt";
-import {LightBulb} from "@styled-icons/octicons/LightBulb";
-import {Grid} from "@styled-icons/boxicons-solid/Grid";
-import * as S from "./styled";
+import { Home } from "@styled-icons/boxicons-solid/Home"
+import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
+import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
+import { LightBulb } from "@styled-icons/octicons/LightBulb"
+import { Grid } from "@styled-icons/boxicons-solid/Grid"
+import * as S from "./styled"
+import { ThList as List } from "@styled-icons/typicons/ThList"
 
-const Menubar = () => (
-  <S.MenubarWrapper>
-    <S.MenubarGroup>
-      <S.MenubarLink to="/" title="Voltar para Home">
-        <S.MenubarItem><Home/></S.MenubarItem>
-      </S.MenubarLink>
-      <S.MenubarLink to="/search" title="Pesquisar">
-        <S.MenubarItem><Search/></S.MenubarItem>
-      </S.MenubarLink>
-    </S.MenubarGroup>
-    <S.MenubarGroup>
-      <S.MenubarItem title="Mudar o tema"><LightBulb/></S.MenubarItem>
-      <S.MenubarItem title="Mudar visualização"><Grid/></S.MenubarItem>
-      <S.MenubarItem title="Ir para o topo"><Arrow/></S.MenubarItem>
-    </S.MenubarGroup>
-  </S.MenubarWrapper>
-)
+const Menubar = () => {
+
+  const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
+
+  const isDarkMode = theme === 'dark'
+  const isListMode = display === 'list'
+
+  useEffect(() => {
+    setTheme(window.__theme)
+    setDisplay(window.__display)
+
+    window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
+  }, [])
+
+  return (
+    <S.MenubarWrapper>
+      <S.MenubarGroup>
+        <S.MenubarLink to="/" title="Voltar para Home">
+          <S.MenubarItem>
+            <Home />
+          </S.MenubarItem>
+        </S.MenubarLink>
+        <S.MenubarLink to="/search" title="Pesquisar">
+          <S.MenubarItem>
+            <Search />
+          </S.MenubarItem>
+        </S.MenubarLink>
+      </S.MenubarGroup>
+      <S.MenubarGroup>
+        <S.MenubarItem
+          title="Mudar o tema"
+          onClick={() => {
+            window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
+          }}
+          className={theme}
+        >
+          <LightBulb />
+        </S.MenubarItem>
+        <S.MenubarItem title="Mudar visualização" onClick={() => {
+          window.__setPreferredDisplay( isListMode ? 'grid' : 'list')
+        }}>
+          { isListMode ? <Grid /> : <List/>}
+        </S.MenubarItem>
+        <S.MenubarItem title="Ir para o topo">
+          <Arrow />
+        </S.MenubarItem>
+      </S.MenubarGroup>
+    </S.MenubarWrapper>
+  )
+}
 
 export default Menubar
