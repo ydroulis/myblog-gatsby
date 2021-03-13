65464bad2fd6b68d6a99190f7ef99e7e28995f95
ydrouulis <lombardi.droulis@gmail.com>
2021-03-13 10:13:22 -0300

    chore: instal deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `Frontend Developer`,
     description: `A blog about frontend development and other stuff.`,
     author: `@myblog`,
+    siteUrl: `https://myblog.com.br`
   },
   flags: {
     THE_FLAG: false,
@@ -75,17 +76,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `MyBlog`,
+        short_name: `MyBlog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
