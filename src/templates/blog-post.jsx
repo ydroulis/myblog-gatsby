d9db3e568a4ab12730b729f71b259d9a9e8b2791
ydrouulis <lombardi.droulis@gmail.com>
2021-03-13 10:15:29 -0300

    chore: set SEO

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,40 +1,46 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from "../components/Layout";
-import SEO from "../components/seo";
-import RecommendedPosts from "../components/RecommendedPosts";
-import Comments from "../components/Comments";
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import RecommendedPosts from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 
-import * as S from "../components/Post/styled";
+import * as S from "../components/Post/styled"
 
 const BlogPost = ({ data, pageContext }) => {
-    const post = data.markdownRemark
-    const next = pageContext.nextPost;
-    const previous = pageContext.previousPost;
+  const post = data.markdownRemark
+  const next = pageContext.nextPost
+  const previous = pageContext.previousPost
 
-    return(
-        <Layout>
-          <SEO title={post.frontmatter.title}/>
-            <S.PostHeader>
-              <S.PostDate>
-                {post.frontmatter.date} • {post.timeToRead} min de leitura
-              </S.PostDate>
-              <S.PostTitle><h1>Title: {post.frontmatter.title}</h1></S.PostTitle>
-              <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
-            </S.PostHeader>
-            <S.MainContent>
-              <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
-            </S.MainContent>
-            <RecommendedPosts next={next} previous={previous}/>
-            <Comments url={post.fields.slug} title={post.frontmatter.title}/>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description}
+        image={post.frontmatter.image}
+      />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>
+          <h1>Title: {post.frontmatter.title}</h1>
+        </S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+      <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
+    </Layout>
+  )
 }
 
 export const query = graphql`
-query Post($slug: String!) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       fields {
         slug
       }
@@ -42,12 +48,12 @@ query Post($slug: String!) {
         title
         description
         date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        image
       }
       html
       timeToRead
     }
   }
-  
 `
 
-export default BlogPost;
+export default BlogPost
