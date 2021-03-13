851544fef9696e03e7cea9bbc22efca4ecea1ce1
ydrouulis <lombardi.droulis@gmail.com>
2021-03-13 12:45:21 -0300

    style: make responsive

--- src/components/SocialLinks/styled.js
+++ src/components/SocialLinks/styled.js
@@ -1,8 +1,14 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const SocialLinksWrapper = styled.nav`
     margin: 2rem auto;
     width: 100%;
+
+    ${media.lessThan("large")`
+    margin: 0.5rem 0 0 0;
+    width: auto;
+    `}
 `
 
 export const SocialLinksList = styled.ul`
@@ -10,9 +16,14 @@ export const SocialLinksList = styled.ul`
     display: flex;
     justify-content: space-around;
     list-style: none;
+    flex-wrap: wrap;
 `
 
-export const SocialLinksItem = styled.li``
+export const SocialLinksItem = styled.li`
+${media.lessThan("large")`
+margin-left: 0.5rem;
+`}
+`
 
 export const SocialLinksLink = styled.a`
     color: var(--texts);
