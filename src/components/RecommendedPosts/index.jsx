6c283d06af2ad4111f18330b713ad52abb1e1c8c
ydrouulis <lombardi.droulis@gmail.com>
2021-03-13 14:30:49 -0300

    feat: add transiton link

--- src/components/RecommendedPosts/index.jsx
+++ src/components/RecommendedPosts/index.jsx
@@ -1,20 +1,35 @@
 import React from "react"
 import propTypes from "prop-types"
 import * as S from "./style"
+import getThemeColor from '../../utils/getThemeColor'
 
 const RecommendedPosts = ({ next, previous }) => (
   <S.RecommendedWrapper>
     {previous && (
-      <S.RecommendedLink to={previous.fields.slug} className="previous">
+      <S.RecommendedLink
+        cover
+        direction="right"
+        bg={getThemeColor().cover}
+        duration={1}
+        to={previous.fields.slug}
+        className="previous"
+      >
         {previous.frontmatter.title}
-        <br/>
+        <br />
         <p>← Anterior</p>
       </S.RecommendedLink>
     )}
     {next && (
-      <S.RecommendedLink to={next.fields.slug} className="next">
+      <S.RecommendedLink
+        cover
+        direction="left"
+        bg={getThemeColor().cover}
+        duration={1}
+        to={next.fields.slug}
+        className="next"
+      >
         {next.frontmatter.title}
-        <br/>
+        <br />
         <p>Próximo →</p>
       </S.RecommendedLink>
     )}
@@ -24,20 +39,20 @@ const RecommendedPosts = ({ next, previous }) => (
 RecommendedPosts.propTypes = {
   next: propTypes.shape({
     frontmatter: propTypes.shape({
-      title: propTypes.string.isRequired
+      title: propTypes.string.isRequired,
     }),
     fields: propTypes.shape({
-      slug: propTypes.string.isRequired
+      slug: propTypes.string.isRequired,
     }),
   }),
   previous: propTypes.shape({
     frontmatter: propTypes.shape({
-      title: propTypes.string.isRequired
+      title: propTypes.string.isRequired,
     }),
     fields: propTypes.shape({
-        slug: propTypes.string.isRequired
-      }),
-  })
+      slug: propTypes.string.isRequired,
+    }),
+  }),
 }
 
 export default RecommendedPosts
